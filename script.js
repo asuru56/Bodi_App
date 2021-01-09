d4eb3fe5c54451fa54aefd97a64f32266e2ea153
asuru <56asuru@gmail.com>
2021-01-09 18:11:36 +0530

    Mediation and Relaxtion App for the mind

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.container')
+const text = document.querySelector('#text')
+
+const totalTime = 9000
+const breatheTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+breatheAnimation()
+
+function breatheAnimation() {
+    text.innerHTML = 'BREATH IN'
+    container.className = "container grow"
+
+    setTimeout(() => {
+        text.innerText = 'HOLD'
+
+        setTimeout(() => {
+            text.innerText = 'BREATH OUT'
+            container.className = 'container shrink'
+        }, holdTime)
+    }, breatheTime)
+
+
+}
+
+setInterval(breatheAnimation, totalTime)
